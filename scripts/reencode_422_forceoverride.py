6e2cc2a9f76664f50d36fa4adc0fe35c333bbe85
Gregory Martin <greg@edx.org>
2018-02-07 15:39:57 -0500

    Upd.

--- scripts/reencode_422_forceoverride.py
+++ scripts/reencode_422_forceoverride.py
@@ -5,13 +5,27 @@ One off script to force re-encode 422 (satellite encoded) videos for iOS/Android
 
 '''
 Normal Command
-ffmpeg -hide_banner -y -i /Users/gregmartin/ENCODE_WORKDIR/UBCSC1SC2017-V007900.mov \
+ffmpeg -hide_banner -y -i ${ENCODE_WORKDIR}/UBCSC1SC2017-V007900.mov \
 -c:v libx264 -vf scale=640:360 -crf 27 -movflags faststart ${ENCODE_WORKDIR}/UBCSC1SC2017-V007900_MB2.mp4
 
 Should Read
--pix_fmt yuv420p
-ffmpeg -hide_banner -y -i /Users/gregmartin/ENCODE_WORKDIR/UBCSC1SC2017-V007900.mov \
--c:v libx264 -vf scale=640:360 -crf 27 -movflags faststart ${ENCODE_WORKDIR}/UBCSC1SC2017-V007900_MB2.mp4
+ffmpeg -hide_banner -y -i ${ENCODE_WORKDIR}/UBCSC1SC2017-V007900.mov \
+-pix_fmt yuv420p -c:v libx264 -vf scale=640:360 -crf 27 -movflags faststart ${ENCODE_WORKDIR}/UBCSC1SC2017-V007900_MB2.mp4
 
 
 '''
+
+import os
+
+
+def download_file():
+    pass
+
+
+def encode_file():
+    pass
+
+
+def upload_file():
+    pass
+
