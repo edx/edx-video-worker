fe1ff847aad370878ec08151f7c62b76bdf8d1dd
Quho <armbox@gmail.com>
2018-11-27 13:48:03 +0900

    On redis, default work time out is 1 hour, so it retries the job if it's not finished within 1 hour even though it's finished successfully after 1 hour.
    Increate the work timeout value.

--- video_worker/celeryapp.py
+++ video_worker/celeryapp.py
@@ -24,6 +24,9 @@ def cel_start():
 
     app.conf.update(
         BROKER_CONNECTION_TIMEOUT=60,
+        BROKER_TRANSPORT_OPTIONS = {
+            'visibility_timeout': 43200
+        },
         CELERY_IGNORE_RESULT=True,
         CELERY_TASK_RESULT_EXPIRES=10,
         CELERYD_PREFETCH_MULTIPLIER=1,
